537b10e3ff0b71749c1369082617e978e282fe71
Grach90 <grach.tver777@gmail.com>
2021-08-02 02:36:27 +0300

    first commit

---
+++ src/schemas/task.schema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose'),
+    mongoosePaginate = require('mongoose-paginate'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.Types.ObjectId;
+
+const TaskSchema = new Schema({
+    owner: {
+        type: ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    main_text: {
+        type: String
+    },
+    author_name: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: 'active',
+        enum: ['active', 'done']
+    },
+    date: {
+        type: Date,
+        format: 'date-time'
+    },
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+TaskSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('Task', TaskSchema);
